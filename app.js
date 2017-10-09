0def8ae18acb38a3c09d6a730e106c89e6432544
Katkina <ejouravleva@gmail.com>
2017-10-09 19:27:01 +0100

    update 1 app.js
    includes some .js code

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-
+var request = new XMLHttpRequest()
+request.addEventListener("load", function(){
+  var data=this.responsetext
+  var json=JSON.parse(data)
+  console.log(json)
+})
+request.open("GET","http://restcountries.eu/rest/v1")
+request.send()
