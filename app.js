5ba5f55dbcc74f47379b015c69f4ae0e14133da3
Katkina <ejouravleva@gmail.com>
2017-10-09 19:38:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,23 @@
-// Our labels and three data series
-var data = {
-  labels: ['Week1', 'Week2', 'Week3', 'Week4', 'Week5', 'Week6'],
-  series: [
-    [5, 4, 3, 7, 5, 10],
-    [3, 2, 9, 5, 4, 6],
-    [2, 1, -3, -4, -2, 0]
-  ]
-};
+https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.js
+fetch('https://restcountries.eu/rest/v2/all')
+.then(getJsonResponse)
+.then(chartData)
 
-// We are setting a few options for our chart and override the defaults
-var options = {
-  // Don't draw the line chart points
-  showPoint: false,
-  // Disable line smoothing
-  lineSmooth: false,
-  // X-Axis specific configuration
-  axisX: {
-    // We can disable the grid for this axis
-    showGrid: false,
-    // and also don't show the label
-    showLabel: false
-  },
-  // Y-Axis specific configuration
-  axisY: {
-    // Lets offset the chart a bit from the labels
-    offset: 60,
-    // The label interpolation function enables you to modify the values
-    // used for the labels on each axis. Here we are converting the
-    // values into million pound.
-    labelInterpolationFnc: function(value) {
-      return '$' + value + 'm';
-    }
-  }
-};
+function getJsonResponse(response){
+  return response.json()
+}
 
-// All you need to do is pass your configuration as third parameter to the chart function
-new Chartist.Line('.ct-chart', data, options);
+function chartData(data){
+
+  constChartData
+
+  console.log(data)
+}
+
+function mapDatatoChartArray(data) {
+
+  return data.map(function(data){
+    return{x:data.name, y:data.population}
+}
+)
+}
